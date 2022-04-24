93168ec6a72626598dc0ebba8461e9b7e4fde828
Snap96 <67959359+Snap96@users.noreply.github.com>
2022-04-24 18:31:21 +0200

    course pages

--- comps/Course.js
+++ comps/Course.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import {
   MdWeb,
   MdOutlineDesignServices,
@@ -7,7 +7,7 @@ import {
 import { AiOutlineAppstoreAdd } from "react-icons/ai";
 import { GiGrowth } from "react-icons/gi";
 import { SiAzuredevops } from "react-icons/si";
-import Link from "next/link"
+import Link from "next/link";
 
 function Course() {
   return (
@@ -18,23 +18,33 @@ function Course() {
       </h2>
       <div className="flex justify-center items-center">
         <div className="grid grid-cols-2 md:mx-0 mx-10 md:grid-cols-3 gap-5 my-20">
-          <Link href={{
-            pathname:"/CourseLayout",
-            query:"Computer Science"
-          }}>
-              <div  className="group flex-col cursor-pointer justify-center items-center border hover:bg-blue-500 hover:shadow-lg  border-blue-100 rounded-md p-8 hover:border-violet-500">
-                <MdWeb className="text-4xl text-gray-600 group-hover:text-white" />
-                <h1 className="text-xl text-gray-800 font-semibold group-hover:text-white my-2">
-                  Computer Science
-                </h1>
-              </div>
+          <Link
+            href={{
+              pathname: "/CourseLayout",
+              query: { courseName: "Computer Science" },
+            }}
+          >
+            <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-blue-500 hover:shadow-lg  border-blue-100 rounded-md p-8 hover:border-violet-500">
+              <MdWeb className="text-4xl text-gray-600 group-hover:text-white" />
+              <h1 className="text-xl text-gray-800 font-semibold group-hover:text-white my-2">
+                Computer Science
+              </h1>
+            </div>
           </Link>
-          <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-blue-500 hover:shadow-lg  border-blue-100 rounded-md p-8 hover:border-violet-500">
-            <AiOutlineAppstoreAdd className="text-4xl text-gray-600 group-hover:text-white" />
-            <h1 className="text-xl text-gray-800 font-semibold group-hover:text-white my-2">
-              Mobile App Development
-            </h1>
-          </div>
+
+          <Link
+            href={{
+              pathname: "/CourseLayout",
+              query: { courseName: "Mobile App Development" },
+            }}
+          >
+            <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-blue-500 hover:shadow-lg  border-blue-100 rounded-md p-8 hover:border-violet-500">
+              <AiOutlineAppstoreAdd className="text-4xl text-gray-600 group-hover:text-white" />
+              <h1 className="text-xl text-gray-800 font-semibold group-hover:text-white my-2">
+                Mobile App Development
+              </h1>
+            </div>
+          </Link>
           <div className="group flex-col cursor-pointer justify-center items-center border hover:bg-blue-500 hover:shadow-lg  border-blue-100 rounded-md p-8 hover:border-violet-500">
             <MdOutlineDesignServices className="text-4xl text-gray-600 group-hover:text-white" />
             <h1 className="text-xl text-gray-800 font-semibold group-hover:text-white my-2">
